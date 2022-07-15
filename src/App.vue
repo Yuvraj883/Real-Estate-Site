bbe61ca1ce20c7c9082a8cbe394126f9ae152549
Yuvraj883 <kingyuvi88@gmail.com>
2022-07-15 15:54:41 +0530

    Inital Commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+<navbar/>
+</template>
+
+<script >
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+import Navbar from './components/Navbar.vue'
+export default{
+  name:'App',
+  components:{
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+<nav>
+    <span>BrandName</span>
+
+    <ul>
+        <li>Rent</li>
+        <li>Buy</li>
+        <li>Sell</li>
+        <li>Manage Property</li>
+        <li>Resources</li>
+    </ul>
+    </nav>
+</template>
+<script>
+export default{
+name:'Navbar'
+}
+</script>
+<style scoped>
+nav{
+    background:white;
+    color:black;
+    display:flex;
+    position:fixed;
+    width:100%;
+}
+ul{
+    display:flex;
+}
+</style>
