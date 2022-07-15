4d03d76f848e93930d369d811b22a0d2ea044c7e
Yuvraj883 <kingyuvi88@gmail.com>
2022-07-15 16:51:07 +0530

    Minor changes

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="card">
+<img src="../assets/house1.webp"/>
+<section>
+<span class="price">$2000</span>
+<span class="per-month">/month</span>
+<div>Address</div>
+<div class="container">
+<div>
+<img src="../assets/bedIcon.png" alt="bed-icon"/>
+3 Beds
+</div>
+<div>
+<img src="../assets/bathtub.png" alt="bathroom-icon"/>
+2 Bathrooms
+</div>
+<div>
+<img src="../assets/square-meter.webp" alt="square-meter-icon"/>
+5x7m<sup>2</sup>
+</div>
+</div>
+
+</section>
+</div>
+</template>
+<script>
+
+</script>
+<style scoped></style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,6 +9,8 @@
         <li>Manage Property</li>
         <li>Resources</li>
     </ul>
+    <button class="login">LOG IN</button>
+    <button class="logout">LOG Out</button>
     </nav>
 </template>
 <script>
@@ -23,8 +25,36 @@ nav{
     display:flex;
     position:fixed;
     width:100%;
+    position:fixed;
+    top: 0;
 }
 ul{
     display:flex;
+    margin-top:0;
+    
+}
+li{
+    list-style-type:none;
+    margin:1em;
+}
+span{
+    display:inline;
+    margin-top:1rem;
+    font-size:larger;
+    font-weight: 900;
+}
+button{
+    border:none;
+
+    margin-left:1em;
+    box-shadow: 0 0 .1em black;
+}
+.login{
+    color:white;
+    background:#4F46E5;
+}
+.logout{
+    color:#4F46E5;
+    background:white;
 }
 </style>
